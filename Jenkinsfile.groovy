62528c6b28b35c6c3bd74f5c3608fe560dfd45e3
saybee <sayaleebhusari@gmail.com>
2020-04-20 12:21:34 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,6 +16,23 @@ pipeline{
       steps{
         echo "Deployment ${WORKSPACE}"
       }
+      script{
+          def TODAY = new Date()
+          println("${TODAY}")
+          def appID = (${BUILD_ID}==null)? TODAY : ${BUILD_ID}
+          def appTag = ${JOB_NAME}
+          def appVer = (${BUILD_NUMBER}==null)?"LOCAL BUILD" : ${BUILD_NUMBER}
+          def folder = fileExists("${WORKSPACE}/tmp")
+          if(!folder){
+            dir("${WORKSPACE}/tmp"){
+              writeFile file : "version.txt",text : "id :${appID}\ntag:${appTag}\nversion:${appVer}\n"
+            }
+          } else{
+          }
+          
+          sh "ls -l ${WORKSPACE}/tmp/version.txt"
+          sh "cat ${WORKSPACE}/tmp/version.txt"
+        }
     }
   }
 }
