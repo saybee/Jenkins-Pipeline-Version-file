5d244b24ed97bbf536343d3e04d2cf4332aef304
saybee <sayaleebhusari@gmail.com>
2020-04-20 13:42:12 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -24,8 +24,7 @@ pipeline{
           def appID = (env.BUILD_ID==null)? TODAY : env.BUILD_ID
           def appTag = env.JOB_NAME
           def appVer = (env.BUILD_NUMBER==null)?"LOCAL BUILD" : env.BUILD_NUMBER
-          def folder = fileExists("${WORKSPACE}/tmp")
-          if(!folder){
+          if(!fileExists("${WORKSPACE}/tmp")){
             dir("${WORKSPACE}/tmp"){
               writeFile file : "version.txt",text : "id :${appID}\ntag:${appTag}\nversion:${appVer}\n"
             }
