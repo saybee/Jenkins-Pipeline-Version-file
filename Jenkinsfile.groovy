a2d81d7c1b2c1f116428ea5224a2ba18a32ed3c8
saybee <sayaleebhusari@gmail.com>
2020-04-19 21:22:53 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,14 @@
-
+pipeline{
+  agent any
+  stages{
+    stage('Compile stage'){
+      echo 'compiling'
+    }
+    stage('Testing'){
+      echo 'Testing'
+    }
+    stage('Deploy') {
+      echo 'Deployment'
+    }
+  }
+}
