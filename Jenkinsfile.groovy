624e60737052d09fce271fe0e395e62617827b1b
saybee <sayaleebhusari@gmail.com>
2020-04-20 13:31:44 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -21,9 +21,9 @@ pipeline{
           echo "BUILD_ID ${BUILD_ID}"
           echo "JOB_NAME ${JOB_NAME}"
           echo "BUILD_NUMBER ${BUILD_NUMBER}"
-          def appID = (${BUILD_ID}==null)? TODAY : ${BUILD_ID}
-          def appTag = ${JOB_NAME}
-          def appVer = (${BUILD_NUMBER}==null)?"LOCAL BUILD" : ${BUILD_NUMBER}
+          def appID = (env.BUILD_ID==null)? TODAY : env.BUILD_ID
+          def appTag = env.JOB_NAME
+          def appVer = (env.BUILD_NUMBER==null)?"LOCAL BUILD" : env.BUILD_NUMBER
           def folder = fileExists("${WORKSPACE}/tmp")
           if(!folder){
             dir("${WORKSPACE}/tmp"){
