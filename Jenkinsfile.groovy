87e2476ad2db6196ea7f2841edf2bdcf36ea950f
saybee <sayaleebhusari@gmail.com>
2020-04-20 14:21:25 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,19 +17,20 @@ pipeline{
         script{
           echo "Deployment ${WORKSPACE}"
           def TODAY = new Date()
-          println("${TODAY}")
-          echo "BUILD_ID ${BUILD_ID}"
-          echo "JOB_NAME ${JOB_NAME}"
-          echo "BUILD_NUMBER ${BUILD_NUMBER}"
-          def appID = (env.BUILD_ID==null)? TODAY : env.BUILD_ID
-          def appTag = env.JOB_NAME
-          def appVer = (env.BUILD_NUMBER==null)?"LOCAL BUILD" : env.BUILD_NUMBER
           def folder = fileExists("${WORKSPACE}/tmp")
           if(folder){
             dir ("${WORKSPACE}/tmp") {
               deleteDir()
             }
           }
+          echo "BUILD_ID ${BUILD_ID}"
+          echo "JOB_NAME ${JOB_NAME}"
+          echo "BUILD_NUMBER ${BUILD_NUMBER}"
+          def appID = (env.BUILD_ID==null)? TODAY : env.BUILD_ID
+          def appTag = env.JOB_NAME
+          def appVer = (env.BUILD_NUMBER==null)?"LOCAL BUILD" : env.BUILD_NUMBER
+          
+          
            dir("${WORKSPACE}/tmp"){
               writeFile file : "version.txt",text : "id :${appID}\ntag:${appTag}\nversion:${appVer}\n"
             }
