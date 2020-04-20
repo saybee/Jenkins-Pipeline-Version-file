426513807ac1c6884e6eda308dc2fc8790ca5a5c
saybee <sayaleebhusari@gmail.com>
2020-04-20 10:15:20 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,16 +17,17 @@ pipeline{
         script{
           echo 'Inside script'
           def TODAY = new Date()
-          def folder = fileExists("${WORKSPACE}/tmp")
-          if(!folder){
-            folder.mkdirs()
-          }
-          def versionFile = project.file("${WORKSPACE}/tmp/version.txt");
           def appID = (${BUILD_ID}==null)? TODAY : ${BUILD_ID}
           def appTag = ${JOB_NAME}
           def appVer = (${BUILD_NUMBER}==null)?"LOCAL BUILD" : ${BUILD_NUMBER}
-          versionFile.delete()
-          versionFile << "id: ${appID}\ntag: ${appTag}\nversion:${appVer}\n"
+          def folder = fileExists("${WORKSPACE}/tmp")
+          if(!folder){
+            dir("${WORKSPACE}/tmp"){
+              writeFile file : "version.txt",text : "id :${appID}\ntag:${appTag}\nversion:${appVer}\n"
+            }
+          } else{
+          }
+          
           sh "ls -l ${WORKSPACE}/tmp/version.txt"
           sh "cat ${WORKSPACE}/tmp/version.txt"
         }
